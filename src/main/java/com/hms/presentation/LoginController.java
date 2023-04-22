009ca176e79436a3c10e5539d559081ae7858b36
Esrom Mulugeta <esrom.mulugeta@gmail.com>
2023-04-22 13:35:43 -0500

    Fixed FXML Issues, Moved Icons to Assets Folder, Implemented Login Page Load

---
+++ src/main/java/com/hms/presentation/LoginController.java
@@ -0,0 +1,21 @@
+package com.hms.presentation;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import org.controlsfx.control.action.Action;
+
+import java.io.IOException;
+
+public class LoginController {
+    @FXML
+    private BorderPane bpMainParent;
+    @FXML
+    protected void onLogin(ActionEvent e) throws IOException {
+
+    }
+}
