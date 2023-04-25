4e01673730eefd88921a8fefa87cb6ed14fec5b5
Esrom Mulugeta <esrom.mulugeta@gmail.com>
2023-04-25 13:33:12 -0500

    Implemented Patient Box Controller

--- src/main/java/com/hms/presentation/UserController.java
+++ src/main/java/com/hms/presentation/UserController.java
@@ -5,14 +5,17 @@ import com.hms.database.PatientDB;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.scene.effect.Glow;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.util.Callback;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -23,13 +26,45 @@ public class UserController {
     @FXML
     private TextField txtSearchKey;
     private Parent patientTable;
+
     private PatientTableController patientTableController;
+    private Parent patientBox;
     private int doctorId, nurseId, selectedPatient;
     private enum Pages{
         PATIENT,
         APPOINTMENT
     };
     private Pages activePage = Pages.PATIENT;
+
+    private EventHandler<ActionEvent> medicalRecordClickHandler = e -> {
+      System.out.println("Medical Record Click");
+    };
+
+    private EventHandler<ActionEvent> medicationClickHandler = e -> {
+      System.out.println("Medication Click");
+    };
+
+    private Callback<TableView<PatientTable>, TableRow<PatientTable>> patientTableRowFactory = tv -> {
+        TableRow<PatientTable> row = new TableRow<>();
+        row.setOnMouseClicked(event -> {
+            if(event.getClickCount() != 1 || row.isEmpty())
+                return;
+            this.selectedPatient = row.getItem().getId();
+
+            this.vboxMain.getChildren().clear();
+            FXMLLoader loader = new FXMLLoader(this.getClass().getResource("patientBox.fxml"));
+            try {
+                this.patientBox = loader.load();
+                ((PatientBoxController) loader.getController()).setEventHandlers(medicalRecordClickHandler, medicationClickHandler);
+                vboxMain.getChildren().add(this.patientBox);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        return row;
+    };
+
     public void initialize() throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("patientTable.fxml"));
         this.patientTable = loader.load();
@@ -84,24 +119,7 @@ public class UserController {
             }
 
             this.patientTableController.setPatientList(patientTableList);
-            this.patientTableController.setRowDoubleClickHandler(tv -> {
-                TableRow<PatientTable> row = new TableRow<>();
-                row.setOnMouseClicked(event -> {
-                    if(event.getClickCount() != 2 || row.isEmpty())
-                        return;
-                    this.selectedPatient = row.getItem().getId();
-
-                    this.vboxMain.getChildren().clear();
-                    FXMLLoader loader = new FXMLLoader(this.getClass().getResource("patientBox.fxml"));
-                    try {
-                        vboxMain.getChildren().add(loader.load());
-                    } catch (IOException ex) {
-                        throw new RuntimeException(ex);
-                    }
-                });
-
-                return row;
-            });
+            this.patientTableController.setRowDoubleClickHandler(this.patientTableRowFactory);
 
             vboxMain.getChildren().add(this.patientTable);
         }
